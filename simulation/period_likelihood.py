cb9f39b10355f5294ecb4e17ab4818cdaa73869f
Kento Masuda <k_masuda@dhcp184.ess.sci.osaka-u.ac.jp>
2021-11-26 17:55:58 +0900

    add files

---
+++ simulation/period_likelihood.py
@@ -0,0 +1,80 @@
+#%% Packages
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.constants import R_jup, R_sun, R_earth, au, M_sun
+import pandas as pd
+from scipy.stats import norm
+from scipy.integrate import quad
+import os, glob
+
+#%%
+import seaborn as sns
+sns.set(style='ticks', font_scale=1.6, font='sans-serif')
+#sns.set_palette('colorblind')
+plt.rcParams["figure.figsize"] = (18,6)
+
+#%%
+logpmin, logpmax = -0.5, 2.5
+Ngrid = 1000
+pgrid = np.logspace(logpmin, logpmax, Ngrid)
+
+#%%
+vunit = (2*np.pi*R_sun/86400).value*1e-3
+def integrand_cosi(r, p, like_vsini):
+    return lambda x: like_vsini(vunit*r/p*np.sqrt(1-x**2))
+
+def p_marg_likelihood(p, like_vsini, like_r, rmin=0, rmax=3.):
+    integrand_r = lambda r: like_r(r) * quad(integrand_cosi(r, p, like_vsini), 0, 1)[0]
+    return quad(integrand_r, rmin, rmax)[0]
+
+#%%
+def pdf_norm(x, mu, sigma):
+    return np.exp(-0.5*(x-mu)**2/sigma**2)/np.sqrt(2*np.pi)/sigma
+
+def get_like_vsini(vsini, sig_vsini):
+    if vsini <= sig_vsini:
+        return lambda x: np.heaviside(2*sig_vsini-x, 0)*(0.5/sig_vsini)
+        #return lambda x: pdf_norm(x, 0, 2*sig_vsini)*2
+    else:
+        c = norm.sf(0, vsini, sig_vsini)
+        return lambda x: pdf_norm(x, vsini, sig_vsini)/c
+
+def compute_pml(d, parr=pgrid):
+    vsini, sig_vsini = d.vsini, 1.
+    r, sig_r = d.rad, d.rade
+    rmin, rmax = max(0, r-5*sig_r), r+5*sig_r
+    like_vsini = get_like_vsini(vsini, sig_vsini)
+    like_r = lambda x: pdf_norm(x, r, sig_r)
+    like_p_marg = np.array([p_marg_likelihood(_p, like_vsini, like_r, rmin=rmin, rmax=rmax) for _p in parr])
+    return parr, like_p_marg
+
+
+#%%
+outdirs = ["sim-lognorm_1_0p2/", "sim-2lognorm_1p1_0p1_0p6_0p1/", "sim-loguni_m0p5_1/"]
+
+#%%
+for outdir in outdirs:
+    files = glob.glob(outdir + "samples.csv")
+    d = pd.read_csv(files[0])
+    print (files)
+    print ("# %d stars without radii."%np.sum(d.rad!=d.rad))
+
+    if "rade" not in d.keys():
+        d["rade"] = 0.5*(d.radep + d.radem)
+
+    for i in range(len(d)):
+        _d = d.iloc[i]
+        kic = int(_d.kepid)
+        print (kic)
+
+        parr, plike = compute_pml(_d)
+        np.savetxt(outdir+"%s_plike.txt"%str(kic).zfill(9), np.array([parr, plike]).T, fmt='%.8e')
+
+        plt.figure()
+        plt.xlabel("$\log_{10}P_\mathrm{rot}$ $\mathrm{(days)}$")
+        plt.ylabel("relative marginal likelihood")
+        plt.xlim(logpmin, logpmax)
+        plt.plot(np.log10(parr), plike, '-')
+        plt.title("KIC %s: radius $%.1f\,R_\odot$, vsini $%.2f\,\mathrm{km/s}$"%(_d.kepid, _d.rad, _d.vsini))
+        plt.savefig(outdir+"%s_plike.png"%str(kic).zfill(9), dpi=200, bbox_inches="tight")
+        plt.close()
---
+++ simulation/simulate_samples.py
@@ -0,0 +1,79 @@
+#%% Packages
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.constants import R_jup, R_sun, R_earth, au, M_sun
+import pandas as pd
+import os
+vunit = (2*np.pi*R_sun/86400).value*1e-3
+
+#%%
+import seaborn as sns
+sns.set(style='ticks', font_scale=1.6, font='sans-serif')
+#sns.set_palette('colorblind')
+plt.rcParams["figure.figsize"] = (18,6)
+
+#%%
+d = pd.read_csv("../main/data/sample_stars.csv")
+len(d)
+
+#%%
+Nsample = 500
+
+#%%
+rads_true = []
+for i in np.random.randint(len(d), size=Nsample):
+    rads_true.append(d.rad[i]*(1+np.random.randn()*d.rade[i]))
+rads_true = np.array(rads_true)
+rads_true
+
+#%%
+bins = np.linspace(0.9, 4.0, 50)
+plt.hist(d.rad, density=True, bins=bins, histtype='step', lw=1)
+plt.hist(rads_true, density=True, bins=bins, histtype='step', lw=1);
+
+#%%
+rade_obs = 0.04 * rads_true
+rads_obs = rads_true + np.random.randn(Nsample) * rade_obs
+
+#%%
+distname = "lognorm"
+distname = "2lognorms"
+distname = "loguni"
+
+#%%
+np.random.seed(123)
+if distname == "lognorm":
+    name = "lognorm_1_0p2"
+    periods = 10**(1.+np.random.randn(Nsample)*0.2)
+elif distname == "2lognorms":
+    name = "2lognorm_1p1_0p1_0p6_0p1"
+    periods = 10**(1.1 + np.random.randn(Nsample)*0.1)
+    periods2 = 10**(0.6 + np.random.randn(Nsample)*0.1)
+    idx2 = np.random.rand(Nsample)<0.25
+    periods[idx2] = periods2[idx2]
+else:
+    name = "loguni_m0p5_1"
+    periods = 10**(0.5 + np.random.rand(Nsample)*0.5)
+
+
+#%%
+plt.xlim(0, 2)
+plt.hist(np.log10(periods[:500]), density=True);
+
+#%%
+np.random.seed(123)
+cosis = np.random.rand(Nsample)
+vsinis_true = vunit * rads_true / periods * np.sqrt(1-cosis**2)
+vsinis_obs = vsinis_true + np.random.randn(Nsample)
+print (np.min(vsinis_true))
+print (np.min(vsinis_obs))
+vsinis_obs[vsinis_obs<0] = 0
+
+#%%
+kics = np.arange(Nsample)
+outdir = "sim-" + name
+if not os.path.exists(outdir):
+    os.system("mkdir %s"%outdir)
+
+#%%
+pd.DataFrame(data={"kepid": kics, "rad": rads_obs, "rade": rade_obs, "rad_true": rads_true, "vsini": vsinis_obs, "vsini_true": vsinis_true, "period": periods, "cosi_true": cosis}).to_csv(outdir+"/samples_rad.csv", index=False)
